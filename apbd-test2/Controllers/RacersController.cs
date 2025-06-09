462f91769db8b8381213b8acb7160ea57e41fad0
s31725 <s31725@pjwstk.edu.pl>
2025-06-09 14:57:26 +0200

    feat: racers controller

---
+++ apbd-test2/Controllers/RacersController.cs
@@ -0,0 +1,32 @@
+﻿using apbd_test2.Exceptions;
+using apbd_test2.Models;
+using apbd_test2.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apbd_test2.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class RacersController : ControllerBase
+{
+    private readonly IDbService _dbService;
+
+    public RacersController(IDbService dbService)
+    {
+        _dbService = dbService;
+    }
+
+    [HttpGet("{racerId}/participations")]
+    public async Task<IActionResult> GetRacers(int racerId)
+    {
+        try
+        {
+            var order = await _dbService.GetRacersParticipationsAsync(racerId);
+            return Ok(order);
+        }
+        catch (NotFoundException e)
+        {
+            return NotFound();
+        }
+    }
+}
