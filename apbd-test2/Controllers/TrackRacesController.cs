b62a6b9b3b66fe1c6e1247b6e6d6d2474e77148b
s31725 <s31725@pjwstk.edu.pl>
2025-06-09 15:18:05 +0200

    feat: track races controller

---
+++ apbd-test2/Controllers/TrackRacesController.cs
@@ -0,0 +1,32 @@
+﻿using apbd_test2.DTOs;
+using apbd_test2.Exceptions;
+using apbd_test2.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apbd_test2.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TrackRacesController : ControllerBase
+{
+    private readonly IDbService _dbService;
+
+    public TrackRacesController(IDbService dbService)
+    {
+        _dbService = dbService;
+    }
+    
+    [HttpPost("participants")]
+    public async Task<IActionResult> AddRacersParticipation(AddRacersPatricipationDTO request)
+    {
+        try
+        {
+            await _dbService.AddRacersParticipationsAsync(request);
+            return Ok();
+        }
+        catch (NotFoundException e)
+        {
+            return NotFound(e.Message);
+        }
+    }
+}
