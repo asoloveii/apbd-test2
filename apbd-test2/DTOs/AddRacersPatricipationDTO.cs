a587def4913310f61f70a0663978fc3fa0d3f452
s31725 <s31725@pjwstk.edu.pl>
2025-06-09 14:41:20 +0200

    feat: dtos completed

---
+++ apbd-test2/DTOs/AddRacersPatricipationDTO.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace apbd_test2.DTOs;
+
+public class AddRacersPatricipationDTO
+{
+    [Required]
+    [MaxLength(50)]
+    public string RaceName { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(100)]
+    public string TrackName { get; set; } = string.Empty;
+
+    [Required]
+    public List<RacersPatricipationDTO> Participations { get; set; } = new List<RacersPatricipationDTO>();
+}
+
+public class RacersPatricipationDTO
+{
+    [Required]
+    public int RacerId { get; set; }
+
+    [Required]
+    public int Position { get; set; }
+
+    [Required]
+    public int FinishTimeInSeconds { get; set; }
+}
---
+++ apbd-test2/DTOs/GetRacersParticipationsDTO.cs
@@ -0,0 +1,31 @@
+﻿namespace apbd_test2.DTOs;
+
+public class GetRacersParticipationsDTO
+{
+    public int RacerId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public List<PatricipationDTO> Patricipations { get; set; }
+}
+
+public class PatricipationDTO
+{
+    public RaceDTO Race { get; set; }
+    public TrackDTO Track { get; set; }
+    public int Laps { get; set; }
+    public int FinishInSeconds { get; set; }
+    public int Position { get; set; }
+}
+
+public class RaceDTO
+{
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public DateTime Date { get; set; }
+}
+
+public class TrackDTO
+{
+    public string Name { get; set; }
+    public decimal LengthInKm { get; set; }
+}
